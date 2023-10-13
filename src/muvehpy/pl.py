2b702efab86036102935a3becfbd1cdaa008b782
Austin Gillen <4809242+agillen@users.noreply.github.com>
2023-10-12 21:28:43 -0600

    add support for custom palettes

--- src/muvehpy/pl.py
+++ src/muvehpy/pl.py
@@ -4,7 +4,7 @@ import scanpy as sc
 import pandas as pd
 import numpy as np
 
-def muveh_plot(adata, variants = [], plot_type = 'genotype', incl_oth = False, genomic_names = False, **kwargs):
+def muveh_plot(adata, variants = [], plot_type = 'genotype', incl_oth = False, genomic_names = False, palette = None, **kwargs):
   from muvehpy.pp import muveh_score_mut, muveh_score, muveh_depth
   variants_sel = adata.uns['muveh_lut'][adata.uns['muveh_lut'].isin(variants).any(axis=1)].drop('variant_name', axis = 1).drop_duplicates()
 
@@ -16,15 +16,18 @@ def muveh_plot(adata, variants = [], plot_type = 'genotype', incl_oth = False, g
     col_names = variants_sel['variant']
   else:
     col_names = variants_sel['plot_name']
+
+  if palette is None:
+    palette = {'0' : '#80808030', '1' : 'yellow', '2' : 'red', '3' : 'purple'}
   
   if plot_type == 'genotype':
     sc.pl.umap(sc.AnnData(obs = pd.concat([adata.obs, pd.DataFrame.sparse.from_spmatrix(muveh_score_mut(adata)[:,variant_indices], \
                           index = adata.obs_names, columns = col_names).astype(str)], axis = 1), obsm = adata.obsm), \
-               color = col_names, palette = {'0' : '#80808050', '1' : 'yellow', '2' : 'red'}, **kwargs)
+               color = col_names, palette = palette, **kwargs)
   elif plot_type == 'genotype3':
     sc.pl.umap(sc.AnnData(obs = pd.concat([adata.obs, pd.DataFrame.sparse.from_spmatrix(muveh_score(adata)[:,variant_indices], \
                           index = adata.obs_names, columns = col_names).astype(str)], axis = 1), obsm = adata.obsm), 
-               color = col_names, palette = {'0' : '#80808050', '1' : 'yellow', '2' : 'red', '3' : 'purple'}, **kwargs)
+               color = col_names, palette = palette, **kwargs)
   elif plot_type == 'depth':
     sc.pl.umap(sc.AnnData(obs = pd.concat([adata.obs, pd.DataFrame.sparse.from_spmatrix(muveh_depth(adata, \
                                                                                         incl_oth = incl_oth)[:,variant_indices], \
